515525c53ef6ec04ff3f86cc195f9eada22b6538
Robert Cornell <robert@alkami.io>
2017-07-29 02:00:35 -0600

    extracted HTTParty action into a service to access routine controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,18 @@
 class UsersController < ApplicationController
-  include HTTParty
 
   before_action :authenticate_user!
 
   def show
     @user ||= User.find_by_id(params[:id])
 
-    response = HTTParty.get('http://localhost:3001/api/v1/routines?api_key=9d4a77caa554451e957755bf771cbe4d').deep_symbolize_keys
-    @routines = response[:data]
+    options =  {
+      query: {
+        api_key: ENV['AF_ROUTINES_API_KEY'],
+        user_id: @user.id
+      }
+    }
+
+    response = RoutinesAPI.new
+    @routines = response.routines(options)[:data]
   end
 end

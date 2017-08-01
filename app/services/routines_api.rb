38515c5627b9ea277849db784f24c179e390a3be
AlkamiDev <robert@alkami.io>
2017-07-31 21:08:49 -0600

    Revert "Revert "Create routines api 0729a""

--- app/services/routines_api.rb
+++ app/services/routines_api.rb
@@ -5,10 +5,33 @@ class RoutinesAPI
   def initalize
   end
 
+  #
+  ## GET All Routines from API
   def routines(options)
     options = options
     self.class.get('/routines', options).deep_symbolize_keys
   end
 
+  #
+  ## Get Single Routine from API
+  def routine(options, routine_id)
+    options = options
+    self.class.get("/routines/#{routine_id}", options).deep_symbolize_keys
+  end
+
+  def new_routine(options)
+    options = options
+    self.class.post('/routines', options)
+  end
+
 
 end
+
+=begin
+HTTParty.post("https://www.gittip.com/#{user}/tips.json",
+  {
+    :body => [ { "amount" => "0.25", "platform" => "gittip", "username" => "whit537" } ].to_json,
+    :basic_auth => { :username => api_key },
+    :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+  })
+=end
